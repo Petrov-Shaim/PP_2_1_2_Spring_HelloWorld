e81a39ab958da2ea2dcf263ca15aa4e927369558
WhtFlm <petrov.shaim.anatoliy@gmail.com>
2023-03-12 16:53:04 +0300

    Cat was added

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String message;
+
+    public String getMau() {
+        return message;
+    }
+
+    public void setMau(String message) {
+        this.message = message;
+    }
+}
